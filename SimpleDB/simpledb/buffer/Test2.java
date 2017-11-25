50e305d9e165bd315f3331a7776503087c7266c0
HuyTu7 <huyqtu7@gmail.com>
2017-11-25 14:35:05 -0500

    update test

--- SimpleDB/simpledb/buffer/Test2.java
+++ SimpleDB/simpledb/buffer/Test2.java
@@ -22,10 +22,8 @@ public class Test2 {
 		SimpleDB.initFileLogAndBufferMgr("test2");
 		int numOfBuffers = 8;
 		BufferMgr buffer = new BufferMgr(numOfBuffers);
-		// Test 1
-		System.out.println(buffer.available());
-		System.out.println();
 		
+		// Test 1
 		Block blk1 = new Block("blk1",1);
 		Block blk2 = new Block("blk2",2);
 		Block blk3 = new Block("blk3",3);
@@ -35,61 +33,102 @@ public class Test2 {
 		Block blk7 = new Block("blk7",7);
 		Block blk8 = new Block("blk8",8);
 		Block blk9 = new Block("blk9",9);
+		Block blk10 = new Block("blk10",10);
+		System.out.println("Created File Blocks.");
 		
 		// Test 2
-		Buffer buf1 = buffer.pin(blk1);
-		System.out.println("Available buffers : " + buffer.available() + "\n");
-		Buffer buf2 = buffer.pin(blk2);
-		System.out.println("Available buffers : " + buffer.available() + "\n");
-		Buffer buf3 = buffer.pin(blk3);
-		System.out.println("Available buffers : " + buffer.available() + "\n");
-		Buffer buf4 = buffer.pin(blk4);
-		System.out.println("Available buffers : " + buffer.available() + "\n");
-		Buffer buf5 = buffer.pin(blk5);
-		System.out.println("Available buffers : " + buffer.available() + "\n");
-		Buffer buf6 = buffer.pin(blk6);
-		System.out.println("Available buffers : " + buffer.available() + "\n");
-		Buffer buf7 = buffer.pin(blk7);
-		System.out.println("Available buffers : " + buffer.available() + "\n");
-		Buffer buf8 = buffer.pin(blk8);
-		System.out.println("Available buffers : " + buffer.available() + "\n");
+		System.out.println("Check the number of available buffers initially: " + buffer.available());
+		System.out.println();
+		
 		
 		// Test 3
-		try {
-			// Should throw exception
-			System.out.println("No available buffer, yet attempting to pin a block.");
-			Buffer buff = buffer.pin(blk8);
-			System.out.println("Unexpected : Exception Expected.");
-		} catch (simpledb.buffer.BufferAbortException e) {
-			// Expected behavior.
-			System.out.println("****** Buffer Abort Exception thrown - TimedOut ******");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		Buffer buf1 = buffer.pin(blk1);
+		System.out.println("Available buffers : " + buffer.available());
+		Buffer buf2 = buffer.pin(blk2);
+		System.out.println("Available buffers : " + buffer.available());
+		Buffer buf3 = buffer.pin(blk3);
+		System.out.println("Available buffers : " + buffer.available());
+		Buffer buf4 = buffer.pin(blk4);
+		System.out.println("Available buffers : " + buffer.available());
+		Buffer buf5 = buffer.pin(blk5);
+		System.out.println("Available buffers : " + buffer.available());
+		Buffer buf6 = buffer.pin(blk6);
+		System.out.println("Available buffers : " + buffer.available());
+		Buffer buf7 = buffer.pin(blk7);
+		System.out.println("Available buffers : " + buffer.available());
+		Buffer buf8 = buffer.pin(blk8);
+		System.out.println("Available buffers : " + buffer.available());
+		Buffer buff = new Buffer();
 		
+		//BufferMgr buffer_copy = buffer;
+		// Test 4
+		Buffer[] newb = new Buffer[2];
+		newb = test4(buffer, buf1, buf2, blk9, blk10);
 		
+		// Test 5
+	    test5(buffer, blk1);
+		
+	    System.out.println("Print: ");
+	    BasicBufferMgr.print();	
+	    
 		buf4 = buffer.pin(blk4);
-		buf2 = buffer.pin(blk2);
+		buf5 = buffer.pin(blk5);
 		buf4 = buffer.pin(blk4);
 		buf7 = buffer.pin(blk7);
-		buf1 = buffer.pin(blk1);
+		
 		buffer.unpin(buf8);
 		buffer.unpin(buf7);
 		buffer.unpin(buf6);
 		buffer.unpin(buf5);
 		buffer.unpin(buf4);
-		buffer.unpin(buf1);
 		buffer.unpin(buf7);
 		buffer.unpin(buf4);
-		buffer.unpin(buf2);
-		buffer.unpin(buf2);
-		Buffer buf9 = buffer.pin(blk9);
+		buffer.unpin(newb[0]);
+		buffer.unpin(newb[0]);
 		//buffer.unpin(buf);
 		
 		BasicBufferMgr.print();
-		
-		
-		
 		System.out.println();
+	}
+	
+	public static void test5(BufferMgr buffer, Block blk) {
+		try {
+			// Should throw exception
+			System.out.println("No available buffer, yet attempting to pin a block.");
+			Buffer newbuff = buffer.pin(blk);
+			BasicBufferMgr.print();
+			System.out.println("Unexpected : Exception Expected.");
+			System.out.println();
+		} catch (simpledb.buffer.BufferAbortException e) {
+			// Expected 
+			System.out.println("****** Buffer Abort Exception thrown - TimedOut ******");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Buffer[] test4(BufferMgr buffer, Buffer buf1, Buffer buf2, Block blk9, Block blk10) {
+		try {
+			System.out.println();
+			System.out.println("Unpinned buffer 1");
+			buffer.unpin(buf1);
+			System.out.println("Unpinned buffer 2");
+			buffer.unpin(buf2);	
+			System.out.println("Available buffers : " + buffer.available());
+			Buffer newbuff1 = buffer.pin(blk9);
+			Buffer newbuff2 = buffer.pin(blk10);
+			System.out.println("Available buffers : " + buffer.available());
+			BasicBufferMgr.print();	
+			System.out.println("Successfully pinned a buffer after unpinning couple of buffers");
+			System.out.println();
+			Buffer[] b = {newbuff1, newbuff2};
+			return b;
+		} catch (simpledb.buffer.BufferAbortException e) {
+			// Unexpected 
+			System.out.println("****** Buffer Abort Exception thrown - TimedOut ******");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
