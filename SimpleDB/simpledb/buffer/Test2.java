2b56f6c01be445df41cc36da5c2f7109bedeef92
HuyTu7 <huyqtu7@gmail.com>
2017-11-25 18:52:20 -0500

    updated format

--- SimpleDB/simpledb/buffer/Test2.java
+++ SimpleDB/simpledb/buffer/Test2.java
@@ -2,22 +2,25 @@ package simpledb.buffer;
 
 import simpledb.file.Block;
 import simpledb.server.SimpleDB;
-/*pin(blk1), pin(blk2), pin(blk3), pin(blk4), pin(blk5),
-pin(blk6), pin(blk7), pin(blk8), pin(blk4), pin(blk2),
-pin(blk7), pin(blk1), unpin(blk8), unpin(blk7),
-unpin(blk6), unpin(blk5), unpin(blk4), unpin(blk1),
-unpin(blk7), unpin(blk4), unpin(blk2), unpin(blk2).
-1 1
-2 0
-3 1
-4 0
-5 0
-6 0
-7 0
-8 0
-*/
+
+/**
+ * added to test Task 1: Buffer Replacement Policy
+ * @author team Q
+ */
 public class Test2 {
 
+	/**
+	 * Assuming the size of bufferpool is 8
+	 * added to test Task 1
+	 *  case 1: Create a list of files-blocks.
+	 *  case 2: Check the number of available buffers initially.
+	 *  case 3: Keep pinning buffers one by one and check the number of available buffers.
+	 *  case 4: Unpin a few buffers and see if you are still getting an exception or not.
+	 *  case 5: When all buffers have been pinned, if pin request is made again, throw an exception
+	 *  case 6: Try to pin a new buffer again, and check your replacement policy 
+	 *          while seeing which currently unpinned buffer is replaced. 
+     * @author team Q
+	 */
 	public static void main(String[] args) {
 		SimpleDB.initFileLogAndBufferMgr("test2");
 		int numOfBuffers = 8;
@@ -35,14 +38,15 @@ public class Test2 {
 		Block blk9 = new Block("blk9",9);
 		Block blk10 = new Block("blk10",10);
 		
-		System.out.println("Created File Blocks.");
+		System.out.println("1. Created File Blocks.");
 		
 		// Test 2
-		System.out.println("Check the number of available buffers initially: " + buffer.available());
+		System.out.println("2. Check the number of available buffers initially: " + buffer.available());
 		System.out.println();
 		
 		
 		// Test 3
+		System.out.println("3. Keep pinning buffers one by one and check the number of available buffers.");
 		Buffer buf1 = buffer.pin(blk1);
 		System.out.println("Available buffers : " + buffer.available());
 		Buffer buf2 = buffer.pin(blk2);
@@ -68,6 +72,8 @@ public class Test2 {
 	    test5(buffer, blk1);
 	    
 	    // Test 6
+	    System.out.println("6. Try to pin a new buffer again, and check your replacement policy \r\n" + 
+				"while seeing which currently unpinned buffer is replaced. ");
 	    buf4 = buffer.pin(blk4);
 		buf3 = buffer.pin(blk3);
 		buf4 = buffer.pin(blk4);
@@ -86,30 +92,41 @@ public class Test2 {
 		buffer.unpin(buf3);
 		buf1 = buffer.pin(blk1);
 		buf2 = buffer.pin(blk2);
-		
 		BasicBufferMgr.print();
 		System.out.println();
 	}
 	
+	/**
+	 * added to test case 5
+	 * @author team Q
+	 */
 	public static void test5(BufferMgr buffer, Block blk) {
 		try {
 			// Should throw exception
-			System.out.println("No available buffer, yet attempting to pin a block.");
+			System.out.println("5. When all buffers have been pinned, if pin request is made again, throw an exception");
+			
 			Buffer newbuff = buffer.pin(blk);
 			BasicBufferMgr.print();
 			System.out.println("Unexpected : Exception Expected.");
 			System.out.println();
 		} catch (simpledb.buffer.BufferAbortException e) {
 			// Expected 
-			System.out.println("****** Buffer Abort Exception thrown - TimedOut ******");
+			System.out.println("Buffer Abort Exception thrown - TimedOut");
+			System.out.println("Successfully throw exception when attempting to pin a block with no available buffer.");
+			System.out.println();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * added to test case 4
+	 * @author team Q
+	 */
 	public static Buffer[] test4(BufferMgr buffer, Buffer buf1, Buffer buf2, Block blk9, Block blk10) {
 		try {
 			System.out.println();
+			System.out.println("4. Unpin a few buffers and see if you are still getting an exception or not.");
 			System.out.println("Unpinned buffer 1");
 			buffer.unpin(buf1);
 			System.out.println("Unpinned buffer 2");
@@ -119,13 +136,13 @@ public class Test2 {
 			Buffer newbuff2 = buffer.pin(blk10);
 			System.out.println("Available buffers : " + buffer.available());
 			BasicBufferMgr.print();	
-			System.out.println("Successfully pinned a buffer after unpinning couple of buffers");
+			System.out.println("Successfully pinned buffers after unpinning a few of them.");
 			System.out.println();
 			Buffer[] b = {newbuff1, newbuff2};
 			return b;
 		} catch (simpledb.buffer.BufferAbortException e) {
 			// Unexpected 
-			System.out.println("****** Buffer Abort Exception thrown - TimedOut ******");
+			System.out.println("Buffer Abort Exception thrown - TimedOut");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
