33a2170d134579196b781920f7e9d6ff830b3de1
cecilia <yxi2@ncsu.edu>
2017-11-29 05:10:26 -0500

    Update TestLogMgr.java

--- SimpleDB/simpledb/log/TestLogMgr.java
+++ SimpleDB/simpledb/log/TestLogMgr.java
@@ -1,5 +1,17 @@
 package simpledb.log;
 
-public class TestLogMgr {
+import java.util.Iterator;
 
+import simpledb.server.SimpleDB;
+
+public class TestLogMgr {
+	
+	public static void main(String[] args) {
+		
+		SimpleDB.initFileMgr("test2");
+		LogMgr logMgr = new LogMgr("testLog");
+		
+		logMgr.append(new Object[] {"1","2","3"});
+		
+	}
 }
