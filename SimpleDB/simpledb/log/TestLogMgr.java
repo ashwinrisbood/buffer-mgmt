ed6ef1df0289dc5f386576ae269a4b66aaa4bd8a
cecilia <yxi2@ncsu.edu>
2017-11-29 23:46:18 -0500

    Update TestLogMgr.java

--- SimpleDB/simpledb/log/TestLogMgr.java
+++ SimpleDB/simpledb/log/TestLogMgr.java
@@ -1,20 +1,32 @@
 package simpledb.log;
 
 import java.util.Iterator;
+import java.nio.ByteBuffer;
 
 import simpledb.server.SimpleDB;
-/**
- * Task 2: newly created for testing logmgr with buffer
- * @author Team number Q
- */
+
 public class TestLogMgr {
 	
 	public static void main(String[] args) {
 		
+		// test1: Log management by Page
+		SimpleDB.initFileMgr("test1");
+		LogMgrOrig logMgrPage = new LogMgrOrig("testLog1");
+		logMgrPage.append(new Object[] {"123"});
+		
+		// test2: Log management by Buffer
 		SimpleDB.initFileMgr("test2");
-		LogMgr logMgr = new LogMgr("testLog");
+		LogMgr logMgrBuffer = new LogMgr("testLog2");
+		logMgrBuffer.append(new Object[] {"123"});
 		
-		logMgr.append(new Object[] {"123"});
+		// Check if Log management by Page & Buffer get the same results
 		
+		ByteBuffer pageByteBuffer = logMgrPage.getByteBuffer();
+		ByteBuffer bufferByteByffer = logMgrBuffer.getByteBuffer();
+		
+		int checkPageBuffer = pageByteBuffer.compareTo(bufferByteByffer);
+		if (checkPageBuffer == 0) {
+			System.out.println("** Log management by Page & Buffer got the same results! **");
+		}
 	}
 }
